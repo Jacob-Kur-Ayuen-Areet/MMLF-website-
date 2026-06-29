bc5f4e949d5fe4fc7739bb36f376a65cc32c68a8
Kur Ayuen <kurjacobayuen@gmail.com>
2026-06-29 15:00:01 +0200

    Add temporary admin setup route for Render deployment

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/setup-admin', function () {
+    $user = \App\Models\User::firstOrCreate(
+        ['email' => 'admin@mmlf.org'],
+        [
+            'name' => 'MMLF Admin',
+            'password' => \Illuminate\Support\Facades\Hash::make('MMLF-Admin-2026!')
+        ]
+    );
+
+    return "Admin user successfully created! Email: admin@mmlf.org | Password: MMLF-Admin-2026!";
+});
