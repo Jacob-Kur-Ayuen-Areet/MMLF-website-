7126e286902ae2b0b883cd50e6ebbf74a74ebb32
Kur Ayuen <kurjacobayuen@gmail.com>
2026-06-29 15:06:23 +0200

    Allow Filament access in production

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,9 +6,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
     use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
